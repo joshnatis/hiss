cc11a181f11f0a8b377611859081ce4723046723
Josh Natis <joshnatis0@gmail.com>
2020-01-09 21:30:38 -0500

    Updates (F, im lazy again)

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,39 +1,42 @@
-let DEFAULT_X = window.innerWidth/5.3;
-let LINE_HEIGHT = 30;
-let PADDING = 16.5;
+const DEFAULT_X = window.innerWidth/5.3;
+const LINE_HEIGHT = 30;
+const PADDING = 16.5;
+const WORD_SPACER = 10;
+let JSMODE = false;
 
 let y;
 let maxInputLength;
 let cnv;
 
-let COLOR = "#33FF00";
-let STDOUT_COLOR = "#FFB000"
-let BG_COLOR = "#282828";
-let BUU_COLOR = "#FB607F"
-let ERROR_COLOR = "#FF0000";
+const INPUT_COLOR = "#33FF00";
+const STDOUT_COLOR = "#FFB000"
+const BG_COLOR = "#282828";
+const BUU_COLOR = "#FB607F"
+const ERROR_COLOR = "#FF0000";
+
+let files = ["blog", "about", "hat", "josh8", "resources"];
+let commands = ["help", "clear", "goto", "ls", "hello", "buu", "js"];
 
 let lines = [];
-let files = ["blog", "about", "hat", "josh8", "resources"];
-let dotfiles = [".vimrc", ".bashrc"];
-let commands = ["help", "clear", "goto", "ls", "hello", "buu"];
+let output_log = [];
 
-function setup() 
+function setup()
 {
 	let w = window.innerWidth;
 	let h = window.innerHeight;
 
 	//MOBILE RESIZING
-    if(displayWidth <= 800) {
-    	cnv = createCanvas(displayWidth, displayHeight);
-    	cnv.position(40,40);
-    	cnv.style("overflow-y", "hidden");
-    	maxInputLength = 27;
-    }
-    else {
-    	cnv = createCanvas(w/1.46, h/1.36);
-    	cnv.position(w/6.4,h/7.5);
-    	maxInputLength = 87;
-    }
+	if(displayWidth <= 800) {
+		cnv = createCanvas(displayWidth, displayHeight);
+		cnv.position(40,40);
+		cnv.style("overflow-y", "hidden");
+		maxInputLength = 27;
+	}
+	else {
+	    cnv = createCanvas(w/1.46, h/1.36);
+	    cnv.position(w/6.4,h/7.5);
+	    maxInputLength = 87;
+	}
 
 	cnv.style("border-radius", "4px");
 	cnv.style("border", "2px solid black");
@@ -43,14 +46,17 @@ function setup()
 	background(BG_COLOR);
 
 	y = floor(window.innerHeight / 6.3); //begin first line at this height
-	createLine(y);
+	//stdout("404: Page Not Found.", y + LINE_HEIGHT - PADDING, DEFAULT_X, false, STDOUT_COLOR);
+	//stdnewline();
+	if(lines.length == 0)
+		createLine(y);
 }
 
-function createLine(y) 
+function createLine(y, prompt="$ ") 
 {
-	displayPrompt(y);
+	displayPrompt(y, prompt);
 
-	let inp = createInput('');
+	let inp = createInput("");
    		inp.elt.focus();
 		inp.position(DEFAULT_X, y);
 
@@ -60,7 +66,7 @@ function createLine(y)
 	  	inp.style("font-size", "1.3em");
 	  	inp.style("outline", "none");
 	  	inp.style("resize", "none");
-	  	inp.style("color", COLOR);
+	  	inp.style("color", INPUT_COLOR);
 	  	inp.style("background-color", "transparent");
 	  	inp.style("border", "none");
 	  	inp.style("font-family", "monospace");
@@ -74,27 +80,47 @@ function createLine(y)
   	lines.push(inp);
 }
 
-function displayPrompt(y) 
+function displayPrompt(y, prompt) 
 {
-	let p = createP("$ ");
-	p.style("color", COLOR);
-	p.style("padding-top", "0.4%");
-  	p.style("padding-bottom", "0.4%");
-	p.style("font-size", "1.3em");
-	p.style("font-family", "monospace");
-	p.position(DEFAULT_X - 20, y - PADDING);
+	let p = createP(prompt);
+		p.style("color", INPUT_COLOR);
+		p.style("padding-top", "0.4%");
+	  	p.style("padding-bottom", "0.4%");
+		p.style("font-size", "1.3em");
+		p.style("font-family", "monospace");
+	p.position(DEFAULT_X - 20, y - PADDING); //position prompt behind input
 }
 
 function draw()
 {
+	if(y + PADDING > cnv.position().y + height)
+		restartAtTop();
+
 	lines[lines.length - 1].elt.focus(); //move cursor into last input
 }
 
 function keyPressed() 
 {
 	if(keyCode == RETURN) {
-    	let content = lines[lines.length - 1].value();
-    	processInput(content);
+		let content = lines[lines.length - 1].value();
+		output_log.push(content);
+
+		if(JSMODE) {
+			if(content == "done") {
+				JSMODE = false;
+			}
+			else {
+				try {
+					eval(content);
+				}
+				catch(err) {
+					stdout(err);
+				}
+			}
+		}
+		else {
+			processInput(content);
+		}
 
     	if(content != "clear") {
     		lines[lines.length - 1].attribute('disabled', '');
@@ -103,23 +129,23 @@ function keyPressed()
     	}
     }
 
-    //KEYBOARD SHORTCUTS
+	//KEYBOARD SHORTCUTS
 
-    if(keyCode == 85) { //u
-    	if(keyIsDown(CONTROL))
-    		lines[lines.length - 1].elt.value = "";
-    } //CTRL U - clear the line
+	if(keyCode == 85) { //u
+    		if(keyIsDown(CONTROL))
+    			lines[lines.length - 1].elt.value = "";
+    	} //CTRL U - clear the line
 
-    if(keyCode == 76) { //l
-    	if(keyIsDown(CONTROL))
-    		reset();
-    } //CTRL L - clears the screen
+	if(keyCode == 76) { //l
+    		if(keyIsDown(CONTROL))
+    			reset();
+    	} //CTRL L - clears the screen
 
-    if(keyCode == UP_ARROW)
-    {
-    	if(lines.length >= 2)
-    		lines[lines.length - 1].elt.value = lines[lines.length - 2].value();
-    } //UP - fills with previous command
+    	if(keyCode == UP_ARROW)
+    	{
+    		if(lines.length >= 2)
+    			lines[lines.length - 1].elt.value = lines[lines.length - 2].value();
+    	} //UP - fills with previous command
 }
 
 function processInput(content)
@@ -129,41 +155,51 @@ function processInput(content)
 
     else if(content == "help")
     {
-    	stdout("Welcome to josh, the OSC Josh Shell version 1.0.");
-    	stdout("Available commands: ");
-    	ls(commands);
-   	}
+	    	let a = stdout("Welcome to ");
+	    	let b = stdout_word("josh", next_word_xpos(a), "blue");
+			stdout(", the OSC JOsh SHell version 1.0", y - PADDING, next_word_xpos(b) - WORD_SPACER, false, STDOUT_COLOR);
+	    	stdout("Available commands: ");
+	    	ls(commands);
+	}
 
-    else if(content.substring(0,4) == "man " || content == "man")
-    	stdout("Nice try :P, go to 'help' instead.");
-
-    else if(content.toLowerCase() == "hi" || content.toLowerCase() == "hello")
-    	stdout("Hello!");
-
-    else if(content == "clear")
+	else if(content == "clear")
     	reset();
 
-   	else if(content.substr(0,5) == "goto ")
+	else if(content.substr(0,5) == "goto ")
    	{
-   		if(files.includes(content.substr(6)))
-   		{
-   			alert(content.substr(6));
-   			window.open("https://josh8.com/" + content.substr(6));
-   		}
-   		alert("uh oh!");
+   		window.open("https://josh8.com/" + content.substr(5), "_self");
    	}
 
-    else if(content == "blog")
-    	window.open("https://josh8.com/blog", "_self");
+	else if(content.substring(0,4) == "man " || content == "man")
+    	stdout("Nice try :P, go to 'help' instead.");
 
-    else
-    {
-    	let y_shorthand = y + LINE_HEIGHT - PADDING;
-    	let failed_cmd = (lines[lines.length - 1].value()).substring(0, 60);
-    	let word = stdout("Sorry, ", y_shorthand, DEFAULT_X, false);
-    	let word2 = stdout(failed_cmd, y_shorthand, word.position().x + word.size().width + 10, false, ERROR_COLOR);
-    	stdout(" is not a valid command.", y_shorthand, word2.position().x + word2.size().width + 10);
-    }
+	else if(content.toLowerCase() == "hello")
+    	stdout("Hello!");
+
+   	else if(content == "goto")
+   		stdout("Well, where should I go? (hint: ls)");
+
+   	else if(content == "buu")
+   		stdout("<3", (y + LINE_HEIGHT - PADDING), DEFAULT_X, true, "blue");
+
+   	else if(content == "")
+   	{}
+
+   	else if(content == "js")
+   	{
+   		stdout("Type away, friend, you're in JavaScript land now.");
+   		stdout("Enter 'done' to return to normal mode.");
+   		JSMODE=true;
+   	}
+
+	else
+	{
+		let y_pos = y + LINE_HEIGHT - PADDING;
+		let failed_cmd = (lines[lines.length - 1].value()).substring(0, 60);
+		let word = stdout("Sorry, ", y_pos, DEFAULT_X, false);
+		let word2 = stdout(failed_cmd, y_pos, next_word_xpos(word), false, ERROR_COLOR);
+		stdout(" is not a valid command.", y_pos, next_word_xpos(word2));
+	}
 }
 
 function stdout(content, y_in=(y + LINE_HEIGHT - PADDING), x_in=DEFAULT_X, newline=true, color_in=STDOUT_COLOR)
@@ -172,21 +208,26 @@ function stdout(content, y_in=(y + LINE_HEIGHT - PADDING), x_in=DEFAULT_X, newli
 		stdnewline();
 
 	let p = createP(content);
-	p.style("color", color_in);
-	p.style("padding-top", "0.4%");
-  	p.style("padding-bottom", "0.4%");
-	p.style("font-size", "1.3em");
-	p.style("font-family", "monospace");
-
+		p.style("color", color_in);
+		p.style("padding-top", "0.4%");
+	  	p.style("padding-bottom", "0.4%");
+		p.style("font-size", "1.3em");
+		p.style("font-family", "monospace");
 	p.position(x_in, y_in);
 
 	return p;
 }
 
+function stdout_word(content, x_in, color_in)
+{
+	let output = stdout(content, y - PADDING, x_in, false, color_in);
+	return output;
+}
+
 function ls(files, flag="")
 {
-	let spacer = 40;
-	let right_margin = 100;
+	let spacer = 40; //distance between each word displayed
+	let right_margin = 100; //allowed distance from right edge of canvas
 
 	let local_x = DEFAULT_X;
 	for(let i = 0; i < files.length; ++i)
@@ -205,9 +246,38 @@ function ls(files, flag="")
 	stdnewline();
 }
 
+function restartAtTop()
+{
+	//delete all elements
+	let text = selectAll('p');
+	text.forEach(t => {
+        t.remove();
+    });
+    let inputs = selectAll('input');
+    inputs.forEach(i => {
+    	i.remove();
+    });
+	
+	//clear screen and reset defaults
+	setup();
+
+	//output previous content on top
+	let content = output_log[output_log.length - 1];
+	y = y - LINE_HEIGHT;
+	processInput(content);
+
+	stdnewline();
+	createLine(y);
+}
+
+function next_word_xpos(word_in)
+{
+	return (word_in.position().x + word_in.size().width + WORD_SPACER);
+}
+
 function reset()
 {
-	y = floor(window.innerHeight / 6.3);
+	lines.splice(0, lines.length);
 	clear();
 	setup();
 }
